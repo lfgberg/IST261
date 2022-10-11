9c4d7142f5f9f3c7551e63584fb6f0cbea87551e
lfgberg <lfgaberg@gmail.com>
2022-10-10 23:27:49 -0400

    progress CSV

--- asg/CsvAppFixMe/src/csvappfixme/CsvAppFixMe.java
+++ asg/CsvAppFixMe/src/csvappfixme/CsvAppFixMe.java
@@ -1,9 +1,15 @@
 package csvappfixme;
 
+import com.opencsv.CSVReaderHeaderAware;
+import com.opencsv.exceptions.CsvValidationException;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -16,15 +22,22 @@ public class CsvAppFixMe {
 
     public static void main(String[] args) {
         List<GeographicRegion> regionList = populateList("RegionsAndAreas.csv");
-        Chooser<GeographicRegion> chooser = new Chooser<>(regionList);
+        //Chooser<GeographicRegion> chooser = new Chooser<>(regionList);
         
-        for (int i = 0; i < 10; i++){
-            System.out.println(chooser.choose());
-        }
+        //for (int i = 0; i < 10; i++){
+        //    System.out.println(chooser.choose());
+        //}
     }
 
-    public static List<GeographicRegion> populateList(String fileName) {
-        Map<String, String> values = new CSVReaderHeaderAware(new FileReader("yourfile.csv")).readMap();
+    public static List<GeographicRegion> populateList(String fileName){
+        try {
+            Map<String, String> values = new CSVReaderHeaderAware(new FileReader(fileName)).readMap();
+        } catch (FileNotFoundException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IOException | CsvValidationException ex ){
+            System.out.println(ex.getMessage());
+        }
+                
         return new ArrayList<>();       
     }
 
